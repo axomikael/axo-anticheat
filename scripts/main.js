2a455e5038b7a729c2bd9ad843823e7f5d6cf154
axomikael <127490345+axomikael@users.noreply.github.com>
2023-07-01 15:24:43 +0300

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+import "charged/banitems.js"
+import "charged/nuker.js"
+import "config.js"
+import "Protect/index.js"
+import "modules/maxstack.js"
+import "config.js"
+import "modules/chat.js"
+import "modules/player.js"
